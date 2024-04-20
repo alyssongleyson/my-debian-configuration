e1a5b12780d918f1d9f94a2cd1530384adddd985
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2024-04-20 18:12:54 -0300

    Initial commit

---
+++ my-debian-configuration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+###################################################################
+# Script Name	:my-debian-configuration.sh
+# Description	:My post-installation configuration of Debian
+# Args			:bash my-debian-configuration.sh
+# Author		:Álysson Gleyson da Silva
+# Email			:
+###################################################################
