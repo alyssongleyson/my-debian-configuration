72a27576eefd3452e2218059b35f92378004dfed
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2024-04-26 23:01:48 -0300

    Finalizing the script

--- my-debian-configuration.sh
+++ my-debian-configuration.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 ###################################################################
-# Script Name	:my-debian-configuration.sh
-# Description	:My post-installation configuration of Debian
-# Args			:bash my-debian-configuration.sh
-# Author		:Álysson Gleyson da Silva
-# Email			:
+# Script Name:	my-debian-configuration.sh
+# Description:	My post-installation configuration of Debian
+# Args:		bash my-debian-configuration.sh
+# Author:	Álysson Gleyson da Silva
+# Email:
 ###################################################################
 
 # Editing sources.list
-
 echo "# deb cdrom:[Debian GNU/Linux 12.5.0 _Bookworm_ - Official amd64 DVD Binary-1 with firmware 20240210-11:28]/ bookworm contrib main non-free-firmware
 
 deb http://deb.debian.org/debian/ bookworm main non-free-firmware
@@ -25,54 +24,43 @@ deb-src http://deb.debian.org/debian/ bookworm-updates main non-free-firmware" >
 
 apt update -y
 apt upgrade -y
-
 ###################################################################
-
 # Installing flatpak and enabling flathub
 apt install flatpak -y
 apt install gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
 ###################################################################
-
 # Removing unnecessary software
-
 apt remove gnome-games gnome-keyring hexchat imagemagick zutty pidgin -y
 apt autoremove -y
-
 ###################################################################
-
 # Installing Bitwarden
-
 flatpak install flathub com.bitwarden.desktop -y
-
 ###################################################################
-
 # Installing Simplenote
-
 flatpak install flathub com.simplenote.Simplenote -y
-
 ###################################################################
-
 # Installing Obsidian
-
 flatpak install flathub md.obsidian.Obsidian -y
-
 ###################################################################
-
 # Installing Brave Browser
-
 flatpak install flathub com.brave.Browser -y
-
 ###################################################################
-
 # Installing Microsoft Edge
-
 flatpak install flathub com.microsoft.Edge -y
-
 ###################################################################
-
 # Installing Mega Sync
-
 wget https://mega.nz/linux/repo/Debian_12/amd64/megasync-Debian_12_amd64.deb && sudo apt install "$PWD/megasync-Debian_12_amd64.deb" -y
 rm megasync-Debian_12_amd64.deb
+###################################################################
+
+echo "
+╭━━━┳╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━━━┳╮
+┃╭━╮┃┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃╭━╮┃┃
+┃┃╱┃┃┃╭╮╱╭┳━━┳━━┳━━┳━╮╱┃┃╱╰┫┃╭━━┳╮╱╭┳━━┳━━┳━╮
+┃╰━╯┃┃┃┃╱┃┃━━┫━━┫╭╮┃╭╮╮┃┃╭━┫┃┃┃━┫┃╱┃┃━━┫╭╮┃╭╮╮
+┃╭━╮┃╰┫╰━╯┣━━┣━━┃╰╯┃┃┃┃┃╰┻━┃╰┫┃━┫╰━╯┣━━┃╰╯┃┃┃┃
+╰╯╱╰┻━┻━╮╭┻━━┻━━┻━━┻╯╰╯╰━━━┻━┻━━┻━╮╭┻━━┻━━┻╯╰╯
+╱╱╱╱╱╱╭━╯┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╰━━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+Finished script" && date
